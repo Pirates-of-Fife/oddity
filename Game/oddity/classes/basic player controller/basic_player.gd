16b1e89e1b2ef765f201d62fed8df71dade50aeb
Zoe McFife <cmdrprotogen27@gmail.com>
2024-09-14 13:10:24 +0200

    added *very* basic player controller

---
+++ Game/oddity/classes/basic player controller/basic_player.gd
@@ -0,0 +1,54 @@
+# This is a basic player controller for testing purposes only.
+
+extends RigidBody3D
+
+@export_category("Mouse Settings")
+@export
+var mouse_sensivity : float = 0.001
+
+var twist_input : float = 0.0
+var pitch_input : float = 0.0
+
+@export_category("Speed")
+@export
+var walk_force : float = 600
+
+@onready
+var twist_pivot : Node3D = $Head/TwistPivot
+
+@onready
+var pitch_pivot : Node3D = $Head/TwistPivot/PitchPivot
+
+
+func _physics_process(delta : float) -> void:
+	if (Input.is_anything_pressed() and Input.mouse_mode != Input.MOUSE_MODE_CAPTURED and mouse_sensivity > 0):
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+	if Input.is_action_just_pressed("player_jump"):
+		apply_central_impulse(global_transform.basis.y * 300)
+
+	var input_dir : Vector2 = Input.get_vector("player_walk_left", "player_walk_right", "player_walk_forwards", "player_walk_backwards")
+	
+	var input_vector : Vector3 = Vector3(input_dir.x, 0, input_dir.y)
+
+	var direction : Vector3 = (twist_pivot.global_transform.basis * input_vector).normalized()
+
+	apply_central_force(direction * walk_force)
+		
+func _process(delta : float) -> void:
+	if Input.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	twist_pivot.rotate_y(twist_input)
+	pitch_pivot.rotate_x(pitch_input)
+	
+	pitch_pivot.rotation.x = clamp(pitch_pivot.rotation.x, deg_to_rad(-90.0), deg_to_rad(90.0))
+	
+	twist_input = 0
+	pitch_input = 0
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			twist_input = - event.relative.x * mouse_sensivity
+			pitch_input = - event.relative.y * mouse_sensivity
