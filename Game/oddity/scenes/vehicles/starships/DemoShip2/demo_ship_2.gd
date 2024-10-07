d5d97f1edf13af4dcec09800e267f5a26b8f268f
Zoe McFife <cmdrprotogen27@gmail.com>
2024-10-07 15:10:39 +0200

    more polishihn

---
+++ Game/oddity/scenes/vehicles/starships/DemoShip2/demo_ship_2.gd
@@ -0,0 +1,10 @@
+extends Starship
+
+class_name DemoShip2
+
+func _process(delta: float) -> void:
+	move_ball()
+
+func move_ball() -> void:
+	$PlayerCam/SpaceBall.position.x = -target_rotational_thrust_vector.y
+	$PlayerCam/SpaceBall.position.y = -target_rotational_thrust_vector.x
