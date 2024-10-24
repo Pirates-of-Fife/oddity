eb2c56eaa3e42f6270af7a381bcdc938adf47e14
Zoe McFife <93198038+ZoeMcFife@users.noreply.github.com>
2024-10-24 12:34:42 +0200

    w.i.p button

--- Game/oddity/scenes/vehicles/starships/demo-ship-2/demo_ship_2.gd
+++ Game/oddity/scenes/vehicles/starships/demo-ship-2/demo_ship_2.gd
@@ -8,3 +8,7 @@ func _process(delta: float) -> void:
 func move_ball() -> void:
 	$Anchor/SpaceBall.position.x = -target_rotational_thrust_vector.y
 	$Anchor/SpaceBall.position.y = -target_rotational_thrust_vector.x
+
+
+func _on_interaction_button_interacted(player: Player, control_entity: ControlEntity) -> void:
+	$DemoControlSeat.
