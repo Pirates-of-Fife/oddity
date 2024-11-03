ff0bb4aa80f9285a27d2929bc59e8c5e6e90c5fd
Zoe McFife <cmdrprotogen27@gmail.com>
2024-11-03 10:06:27 +0100

    labels...

--- Game/oddity/scenes/vehicles/starships/demo-ship-2/health_label.gd
+++ Game/oddity/scenes/vehicles/starships/demo-ship-2/health_label.gd
@@ -8,4 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	text = str((get_parent_node_3d() as Starship).health)
+	text = str((get_parent_node_3d().get_parent_node_3d() as Starship).health)
--- Game/oddity/scenes/vehicles/starships/demo-ship-2/speed_label.gd
+++ Game/oddity/scenes/vehicles/starships/demo-ship-2/speed_label.gd
@@ -8,4 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	text = str((get_parent_node_3d() as Starship).relative_linear_velocity.length())
+	text = str((get_parent_node_3d().get_parent_node_3d() as Starship).relative_linear_velocity.length())
