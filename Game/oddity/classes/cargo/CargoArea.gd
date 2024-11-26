daae49ef742680dd5c3bace6e731c06d8692591a
Zoe McFife <93198038+ZoeMcFife@users.noreply.github.com>
2024-11-26 12:41:59 +0100

    highlights

--- Game/oddity/classes/cargo/CargoArea.gd
+++ Game/oddity/classes/cargo/CargoArea.gd
@@ -7,8 +7,8 @@ var cargo_in_area : CargoContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var error_enter : Error = self.connect("_on_body_entered", body_entered)
-	var error_exit : Error = self.connect("_on_body_exited", body_exited)
+	var error_enter : Error = self.connect("body_entered", body_entered)
+	var error_exit : Error = self.connect("body_exited", body_exited)
 
 	if (error_enter != OK):
 		printerr(str(self) + " failed to connect to body_entered." + str(error_enter))
@@ -22,16 +22,19 @@ func _process(delta: float) -> void:
 	if cargo_in_area == null:
 		return
 
-	if cargo_in_area.is_being_held:
+	if cargo_in_area.is_being_held and cargo_in_area.nearest_cargo_area == self:
 		$MeshInstance3D.show()
 	else:
 		$MeshInstance3D.hide()
 
+
 func body_entered(body: Node3D) -> void:
 	if body is CargoContainer:
 		cargo_in_area = body
+		cargo_in_area.in_cargo_areas.append(self)
 
 func body_exited(body: Node3D) -> void:
 	if body is CargoContainer:
+		cargo_in_area.in_cargo_areas.erase(self)
 		cargo_in_area = null
 		$MeshInstance3D.hide()
--- Game/oddity/classes/cargo/CargoContainer.gd
+++ Game/oddity/classes/cargo/CargoContainer.gd
@@ -7,6 +7,9 @@ class_name CargoContainer
 @export
 var container_size : CargoUnit.ContainerSize
 
+var in_cargo_areas : Array = Array()
+
+var nearest_cargo_area : CargoArea = null
 
 # INFO Will be replaced by a resource file or something else
 @export
@@ -15,9 +18,35 @@ var contents : String
 func _ready() -> void:
 	_cargo_container_ready()
 
+func _process(delta: float) -> void:
+	_cargo_container_process(delta)
+
 func _cargo_container_ready() -> void:
 	_default_ready()
 	_initialize_collision_shape()
+
+func _cargo_container_process(delta : float) -> void:
+	_default_process(delta)
+
+	if is_being_held and in_cargo_areas.size() > 0:
+		find_nearest_cargo_area()
+
+func find_nearest_cargo_area() -> void:
+	var closest_area : CargoArea = null
+	var shortest_distance : float = INF  # Start with a very large number (infinity)
+
+	# Loop through all the cargo areas in the container's `in_cargo_areas` list
+	for area : CargoArea in in_cargo_areas:
+		if area != null:
+		# Calculate the distance between the cargo container and this cargo area
+			var distance : float = global_position.distance_to(area.global_position)
+
+			# If this area is closer than the current closest, update
+			if distance < shortest_distance:
+				shortest_distance = distance
+				closest_area = area
+
+	nearest_cargo_area = closest_area
 
 func _initialize_collision_shape() -> void:
 	var box_shape : BoxShape3D = BoxShape3D.new()
