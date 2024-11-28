0915095c0c6f991b1c990f26cf2dad4b289afa78
Zoe McFife <93198038+ZoeMcFife@users.noreply.github.com>
2024-11-28 14:44:04 +0100

    stuff working hopefully

--- Game/oddity/classes/module/module-slot/dynamic-module-slot/DynamicModuleSlot.gd
+++ Game/oddity/classes/module/module-slot/dynamic-module-slot/DynamicModuleSlot.gd
@@ -1,3 +1,4 @@
+@tool
 extends ModuleSlot
 
 class_name DynamicModuleSlot
@@ -5,30 +6,63 @@ class_name DynamicModuleSlot
 @export
 var module : Module
 
+var module_held_in_slot : Module
+
 @export
 var initialize_area_automatically : bool = true
+
+var highlight_box : Node3D
+@export
+var area_root : Node3D
+
+@export
+var initialize_area_in_editor : bool :
+	set(value):
+		_initialize_area()
 
 func _ready() -> void:
 	_dynamic_module_slot_ready()
 
 func _dynamic_module_slot_ready() -> void:
 	_module_slot_ready()
-	
+
 	if initialize_area_automatically:
 		_initialize_area()
 
+func _process(delta: float) -> void:
+	_dynamic_module_slot_process(delta)
+
+
+
+func _dynamic_module_slot_process(delta : float) -> void:
+	_module_slot_process()
+
+	if module_held_in_slot == null:
+		if highlight_box != null:
+			highlight_box.hide()
+		return
+
+	if module_held_in_slot.is_being_held:
+		if highlight_box != null:
+			highlight_box.show()
+	else:
+		highlight_box.hide()
+
+	if !module_held_in_slot.is_being_held:
+		if module_held_in_slot.module_slot == null:
+			module_held_in_slot.insert(self)
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is Module:
-		if _module_fits(body) == true: 
-			if body.is_being_held_after_uninsert == false:
-				if body.module_slot == null:
-					body.on_game_entity_drop_request.emit()
-					body.insert(self)
+		if _module_fits(body) == true:
+			module_held_in_slot = body
+
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
 	if body is Module:
-		body.is_being_held_after_uninsert = false
+		if body == module_held_in_slot:
+			module_held_in_slot = null
+
 
 func _module_fits(module : Module) -> bool:
 	return false
--- Game/oddity/classes/module/module-slot/dynamic-module-slot/component-slot/ThrusterSlot.gd
+++ Game/oddity/classes/module/module-slot/dynamic-module-slot/component-slot/ThrusterSlot.gd
@@ -37,15 +37,34 @@ func _thruster_slot_ready() -> void:
 	initialize_area_automatically = false
 	_dynamic_module_slot_ready()
 	add_to_group("ComponentSlot")
-		
+
 	var area : Area3D = $SlotArea
-	
+
 	area.collision_layer = 524288
 	area.collision_mask = 524288
-		
+
+
+	if highlight_box == null:
+
+		var mesh_instance : MeshInstance3D = MeshInstance3D.new()
+		var box_mesh : BoxMesh = BoxMesh.new()
+		box_mesh.size = area.get_child(0).size
+		mesh_instance.mesh = box_mesh
+
+		# Assign the highlight material
+		var highlight_material : Material = preload("res://classes/cargo/CargoHighlightMaterial.tres")
+		mesh_instance.material_override = highlight_material
+		mesh_instance.visible = false  # Hide by default
+
+		# Add MeshInstance3D as a child of the Area3D
+		area.add_child(mesh_instance)
+		mesh_instance.owner = get_tree().edited_scene_root
+
+		highlight_box = mesh_instance
+
 	var error_enter : Error = area.body_entered.connect(_on_area_3d_body_entered)
 	var error_exit : Error = area.body_exited.connect(_on_area_3d_body_exited)
-	
+
 	if (error_enter != OK):
 		printerr("Component Slot area enter failed to connect.")
 	if (error_exit != OK):
