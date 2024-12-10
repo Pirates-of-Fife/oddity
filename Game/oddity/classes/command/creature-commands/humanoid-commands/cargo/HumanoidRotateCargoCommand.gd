19bfb287ab6a21acc19c199593214d98d3e99138
Zoe McFife <93198038+ZoeMcFife@users.noreply.github.com>
2024-12-10 11:12:37 +0100

    Revert "still not working ugh"
    This reverts commit 329ce18acdb0974d360ee72acda8b3cc5e0c39b4.

--- Game/oddity/classes/command/creature-commands/humanoid-commands/cargo/HumanoidRotateCargoCommand.gd
+++
@@ -1,8 +0,0 @@
-extends HumanoidCommand
-
-class_name HumanoidRotateCargoCommand
-
-func execute(control_entity : ControlEntity, data : Object = null) -> void:
-	if control_entity is Humanoid:
-		if control_entity.game_entity_being_picked_up is CargoContainer:
-			control_entity.game_entity_being_picked_up.rotate_cargo()
