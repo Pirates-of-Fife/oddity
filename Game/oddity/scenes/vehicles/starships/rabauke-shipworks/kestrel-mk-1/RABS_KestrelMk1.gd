50530b4c3bcce057159d1d17effcec2e84469aa2
Zoe McFife <cmdrprotogen27@gmail.com>
2024-12-28 08:15:55 +0100

    fixed

--- Game/oddity/scenes/vehicles/starships/rabauke-shipworks/kestrel-mk-1/RABS_KestrelMk1.gd
+++ Game/oddity/scenes/vehicles/starships/rabauke-shipworks/kestrel-mk-1/RABS_KestrelMk1.gd
@@ -20,11 +20,15 @@ func _process(delta: float) -> void:
 	velocity_mfd.max_velocity = ship_info.max_linear_velocity
 	velocity_mfd.current_max_velocity = current_max_velocity
 	velocity_mfd.throttle = target_thrust_vector.z
-	velocity_mfd.velocity = local_linear_velocity.length()
-	
+	velocity_mfd.velocity = local_linear_velocity.length()	
 
 
 func toggle_landing_gear() -> void:
+	if $Exterior/LandingGear/RabsKestrelMk1LandingGear.state != 0 and $Exterior/LandingGear/RabsKestrelMk1LandingGear.state != 1:
+		print($Exterior/LandingGear/RabsKestrelMk1LandingGear.state)
+		print("wrong")
+		return
+	
 	$Exterior/LandingGear/RabsKestrelMk1LandingGear.toggle_open_state()
 	$Exterior/LandingGear/RabsKestrelMk1LandingGear2.toggle_open_state()
 	$Exterior/LandingGear/RabsKestrelMk1LandingGear3.toggle_open_state()
@@ -32,3 +36,8 @@ func toggle_landing_gear() -> void:
 	$LandingCollision.disabled = !$LandingCollision.disabled
 	$LandingCollision2.disabled = !$LandingCollision2.disabled
 	$LandingCollision3.disabled = !$LandingCollision3.disabled
+	
+	if $Interior/Bridge/LandingGearLabel.visible:
+		$Interior/Bridge/LandingGearLabel.hide()
+	else:
+		$Interior/Bridge/LandingGearLabel.show()
