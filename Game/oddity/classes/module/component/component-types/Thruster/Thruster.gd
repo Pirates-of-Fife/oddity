997397aecf49b58d494fb7879eea6323e980104f
Zoe McFife <93198038+ZoeMcFife@users.noreply.github.com>
2025-01-16 10:49:45 +0100

    thruster particles

--- Game/oddity/classes/module/component/component-types/Thruster/Thruster.gd
+++ Game/oddity/classes/module/component/component-types/Thruster/Thruster.gd
@@ -5,9 +5,35 @@ class_name Thruster
 @export
 var size : ModuleSize.ThrusterSize
 
+@export_category("Thruster")
+
+@export
+var current_thrust : float :
+	set(value):
+		current_thrust = clampf(value, 0, 1)
+
+		if thruster_particles != null:
+			thruster_particles.amount_ratio = current_thrust
+	get:
+		return current_thrust
+
+@export
+var thruster_particles : GPUParticles3D
+
+@onready
+var initial_direction : Vector3
+
+func _process(delta: float) -> void:
+	_default_process(delta)
+
+	thruster_particles.process_material.set("direction", initial_direction * global_basis.inverse())
+	debug_log(initial_direction * global_basis.inverse())
+	debug_log(thruster_particles.process_material.get("direction"))
+
 func _ready() -> void:
 	_thruster_ready()
 
 func _thruster_ready() -> void:
 	_module_ready()
-	
+
+	initial_direction = thruster_particles.process_material.get("direction")
