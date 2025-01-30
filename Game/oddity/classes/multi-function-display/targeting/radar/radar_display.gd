ae849b86f3c3f6e1106e83d770525837b3349c79
Zoe McFife <93198038+ZoeMcFife@users.noreply.github.com>
2025-01-30 11:55:15 +0100

    asdfgnm,.

--- Game/oddity/classes/multi-function-display/targeting/radar/radar_display.gd
+++ Game/oddity/classes/multi-function-display/targeting/radar/radar_display.gd
@@ -1,5 +1,3 @@
-@tool
-
 extends Node3D
 
 class_name RadarDisplay
@@ -26,10 +24,18 @@ func _ready() -> void:
 	radar_focus = starship.radar_focus_area
 	radar_surrounding = starship.radar_surrounding_area
 
-func _process(delta: float) -> void:
-	var relative_position : Vector3 = target_position_relative_to_ship / 1000
+#func _process(delta: float) -> void:
+#	return
 
-	$RadarBlips/RadarBlip.position = relative_position
+#	var relative_position: Vector3 = target_position_relative_to_ship
+#	var distance: float = relative_position.length()
+
+	# Apply logarithmic scaling
+#	var scaled_distance : float = log(1 + distance) / log(2)  # Base 2 logarithm scaling
+#	var direction : Vector3 = relative_position.normalized()
+
+#	# Scale down by a factor to fit within radar bounds
+#	$RadarBlips/RadarBlip.position = (direction * scaled_distance) / 10
 
 func spawn_radar_blip(game_entity : GameEntity) -> void:
 	var radar_blip : Node3D
@@ -42,13 +48,22 @@ func spawn_radar_blip(game_entity : GameEntity) -> void:
 	else:
 		radar_blip = radar_blip_scene.instantiate()
 
+	var relative_position: Vector3 = starship.global_position - game_entity.global_position
+
+	#print(relative_position)
+
+	var distance: float = relative_position.length()
+
+	# Apply logarithmic scaling
+	var scaled_distance : float = log(1 + distance) / log(2)  # Base 2 logarithm scaling
+	var direction : Vector3 = relative_position.normalized()
+
 	$RadarBlips.add_child(radar_blip)
 
-	var relative_position : Vector3 = (game_entity.global_position - starship.global_position) / 200
+	# Scale down by a factor to fit within radar bounds
+	radar_blip.position = (direction * scaled_distance) / 15
 
-	radar_blip.position = relative_position
-
-
+	print(radar_blip.position)
 
 
 func _on_timer_timeout() -> void:
@@ -57,3 +72,4 @@ func _on_timer_timeout() -> void:
 
 	for i : GameEntity in radar_surrounding.entities:
 		spawn_radar_blip(i)
+		#print("Spawn")
