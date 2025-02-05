0bbaa42fa6512fb951229b6ab18a2db75948b477
Zoe McFife <cmdrprotogen27@gmail.com>
2025-02-05 15:05:10 +0100

    Abyssal sounds cape

--- Game/oddity/classes/abyss/abyssal-tunnel/AbyssalTunnel.gd
+++ Game/oddity/classes/abyss/abyssal-tunnel/AbyssalTunnel.gd
@@ -20,10 +20,17 @@ var starship_movement_step : float = 1
 
 var world : World
 
+@export
+var abyssal_ambiance_scene : PackedScene = preload("res://classes/abyss/abyss/AbyssalAmbiance.tscn")
+
+var abyssal_ambiance : AbyssalAmbiance
 
 func _ready() -> void:
 	tunnel_material = tunnel_mesh.get_active_material(0)
 	world = get_tree().get_first_node_in_group("World")
+	abyssal_ambiance = abyssal_ambiance_scene.instantiate()
+	world.add_child(abyssal_ambiance)
+	$EnterExitPlayer.play()
 
 func _process(delta: float) -> void:
 	tunnel_material.uv1_offset.z += offset_velocity * delta
@@ -37,7 +44,9 @@ func _physics_process(delta: float) -> void:
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	world.unload_tunnel(self)
 	$StaticBody3D/Exit.set_deferred("monitoring", false)
-
+	abyssal_ambiance.stop_playing()
+	$EnterExitPlayer.play_exit()
+	
 func _on_entrance_body_entered(body: Node3D) -> void:
 	if body is Starship:
 		starship = body
