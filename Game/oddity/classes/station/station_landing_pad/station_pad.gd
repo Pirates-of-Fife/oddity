fde2195019f5741389c3c9260b43ebc6e92445ab
Zoe McFife <cmdrprotogen27@gmail.com>
2025-02-06 13:07:07 +0100

    tutorial

--- Game/oddity/classes/station/station_landing_pad/station_pad.gd
+++ Game/oddity/classes/station/station_landing_pad/station_pad.gd
@@ -10,3 +10,10 @@ func _on_landing_pad_starship_landed(starship: Starship) -> void:
 
 func _on_landing_pad_starship_took_off(starship: Starship) -> void:
 	station.player_near_station()
+
+
+func _on_interaction_button_interacted(player: Player, control_entity: ControlEntity) -> void:
+	if $Tutorial.visible:
+		$Tutorial.hide()
+	else:
+		$Tutorial.show()
