85dd5c9168b0ee1388b91f46eab8ee8540c5f8ab
Zoe McFife <93198038+ZoeMcFife@users.noreply.github.com>
2025-04-25 13:54:51 +0200

    thruster

--- Game/oddity/classes/module/component/component-types/thruster/Thruster.gd
+++ Game/oddity/classes/module/component/component-types/thruster/Thruster.gd
@@ -14,13 +14,25 @@ var thruster_sound : AudioStreamPlayer3D
 var modifier : float = 1
 
 @export
+var thruster_heat_curve : Curve
+
+@export
+var thruster_heat_multiplier : float
+
+var heat_timer : Timer = Timer.new()
+
+var current_heat : float :
+	get:
+		return thruster_heat_curve.sample(current_thrust) * thruster_heat_multiplier
+
+@export
 var current_thrust : float :
 	set(value):
 		current_thrust = clampf(value, 0, 1)
 
 		if thruster_particles != null:
 			thruster_particles.amount_ratio = current_thrust
-		
+
 		if thruster_sound != null:
 			thruster_sound.volume_db = lerpf(thruster_sound.volume_db, lerpf(-20, 20, current_thrust * modifier), 0.2)
 			if current_thrust == 0:
@@ -50,6 +62,17 @@ func _thruster_ready() -> void:
 
 	initial_direction = thruster_particles.process_material.get("direction")
 	uninserted.connect(_on_uninsert_thruster)
+
+	heat_timer.one_shot = false
+	heat_timer.timeout.connect(heat_timer_timeout)
+	heat_timer.wait_time = 0.3
+	add_child(heat_timer)
+	heat_timer.start()
+
+func heat_timer_timeout() -> void:
+	if module_slot != null:
+		module_slot.vehicle.add_heat(current_heat)
+
 
 func _on_uninsert_thruster(slot : ModuleSlot) -> void:
 	current_thrust = 0
