b5db235acc8ccd7fb26ebadb551d0676c176c78b
Zoe <93198038+ZoeMcFife@users.noreply.github.com>
2025-06-22 19:12:16 +0200

    wa

--- Game/oddity/classes/station/SpaceStation.gd
+++ Game/oddity/classes/station/SpaceStation.gd
@@ -32,7 +32,7 @@ var enforce_speed_limit : bool = false
 
 func _ready() -> void:
 	_space_station_ready()
-	
+
 func _space_station_ready() -> void:
 	station_label_1.text = station_name
 	station_label_2.text = station_name
@@ -51,25 +51,25 @@ func _on_player_exit_station(body : Node3D) -> void:
 
 	for i : Node3D in station_frame_of_reference.bodies_in_reference_frame:
 		if i is Creature or i is Starship:
-			rotate_on = false	
-			
+			rotate_on = false
+
 func player_near_station() -> void:
 	if !donau_walzer_player.playing:
 		donau_walzer_player.resume_playing()
 
 func player_stop_music() -> void:
 	donau_walzer_player.pause_playing()
-	
+
 func _process(delta: float) -> void:
 	_space_station_process(delta)
 
 func _space_station_process(delta : float) -> void:
 	if rotate_on:
 		rotate(Vector3(1, 0, 0) * global_basis.inverse(), rotation_speed * delta)
-	
+
 	if enforce_speed_limit:
 		var player : Player = get_tree().get_first_node_in_group("Player")
-		
+
 		if player.control_entity is Starship:
 			var ship : Starship = player.control_entity
 			if ship.current_max_velocity > 100:
@@ -77,7 +77,7 @@ func _space_station_process(delta : float) -> void:
 
 func _on_space_station_zone_activate(player: Player, control_entity: ControlEntity) -> void:
 	player_near_station()
-		
+
 	if player.control_entity is Starship:
 		var ship : Starship = player.control_entity
 		ship.radio_chatter.start_playing()
@@ -98,10 +98,10 @@ func _on_player_force_speed_limit_deactivate(player: Player, control_entity: Con
 
 func _on_tree_exiting() -> void:
 	var p : Player = get_tree().get_first_node_in_group("Player")
-	
+
 	if p == null:
 		return
-	
+
 	var c : ControlEntity = p.control_entity
 	if c is Starship:
 		c.radio_chatter.stop_playing()
@@ -118,6 +118,6 @@ func _on_player_detection_zone_activate(player: Player, control_entity: ControlE
 
 func _on_player_detection_zone_deactivate(player: Player, control_entity: ControlEntity) -> void:
 	var i : Node3D = $InteriorRoot.get_child(0)
-	
+
 	if i != null:
 		i.queue_free()
