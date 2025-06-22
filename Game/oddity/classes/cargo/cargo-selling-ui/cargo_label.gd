1d81c53d21bde51f2375d71aa7514f8a873ea2dd
Zoe <93198038+ZoeMcFife@users.noreply.github.com>
2025-06-22 18:14:56 +0200

    being cool
    worked on the cooler module

--- Game/oddity/classes/cargo/cargo-selling-ui/cargo_label.gd
+++ Game/oddity/classes/cargo/cargo-selling-ui/cargo_label.gd
@@ -9,5 +9,5 @@ func set_cargo_text(cargo : CargoContainer) -> void:
 
 func set_module_text(module : Module) -> void:
 	var component_resource : ComponentResource = (module.module_resource as ComponentResource)
-	
+
 	text = component_resource.manufacturer + " " + component_resource.model + " : " + credit_hud.convert_to_human_readable(module.value) + " Credits"
