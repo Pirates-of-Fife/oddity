cd8cd7719633f0acb8db92669324384024b8730a
Zoe <93198038+ZoeMcFife@users.noreply.github.com>
2025-08-03 11:30:23 +0200

    bug spotted

--- Game/oddity/classes/game-entity/static-game-entity/minable/MineableSpawner.gd
+++ Game/oddity/classes/game-entity/static-game-entity/minable/MineableSpawner.gd
@@ -17,25 +17,25 @@ func spawn_planet_surface_sectors() -> void:
 	var i : int = 0
 	for sector : ScatterSector in sectors:
 		i += 1
-		#var sector_node : MineableSector = MineableSector.new()
-		var sector_node : Node3D = Node3D.new()
+		var sector_node : MineableSector = MineableSector.new()
+		#var sector_node : Node3D = Node3D.new()
 		root.add_child(sector_node)
 		
 		sector_node.owner = get_tree().edited_scene_root
 		sector_node.name = "Sector_%d" % i
 		var average : Vector3 = sector.average_position
-		#sector_node.mineable_resource = mineable_resource
+		sector_node.mineable_resource = mineable_resource
 		
-		#var sector_node_root : Node3D = Node3D.new()
-		#sector_node.add_child(sector_node_root)
-		#sector_node.update_rate = 0.5
-		#sector_node.mineable_spawn_root = sector_node_root
-		#sector_node_root.owner = get_tree().edited_scene_root
+		var sector_node_root : Node3D = Node3D.new()
+		sector_node.add_child(sector_node_root)
+		sector_node.update_rate = 0.5
+		sector_node.mineable_spawn_root = sector_node_root
+		sector_node_root.owner = get_tree().edited_scene_root
 		sector_node.position = average
 		
 		var mmi : MultiMeshInstance3D = MultiMeshInstance3D.new()
-		sector_node.add_child(mmi)
-		#root_node.add_child(mmi)
+		#sector_node.add_child(mmi)
+		sector_node_root.add_child(mmi)
 		mmi.owner = get_tree().edited_scene_root
 		#mmi.custom_aabb = AABB(Vector3(-10000,-10000,-10000), Vector3(20000,20000,20000))
 		#mmi.custom_aabb = calculate_multimesh_aabb(mineable_resource.low_detail_mesh, sector.positions)
@@ -52,7 +52,7 @@ func spawn_planet_surface_sectors() -> void:
 		
 		var j : int = 0
 		for t : Transform3D in sector.transforms:
-			#sector_node.append_transform(t)
+			#sector_node.append_transform(t)                                        # THIS LINE CAUSES THE PRE CACHING BUG
 			#t.origin = mmi.to_local(t.origin)
 			var local_t : Transform3D = Transform3D(t)
 			local_t.origin = sector_node.to_local(local_t.origin)
@@ -60,20 +60,19 @@ func spawn_planet_surface_sectors() -> void:
 			j += 1
 			
 		mmi.multimesh = mm
-		print(mmi.get_aabb())
+		#print(mmi.get_aabb())
 
-		#var average : Vector3 = calculate_average_positions(sector_node.mineable_transforms)
-		#var bounding_radius : float = calculate_bounding_radius(sector_node.mineable_transforms) + sector_node.spawn_radius
+		var bounding_radius : float = sector.bounding_radius + sector_node.spawn_radius
 		
-		#var zone : PlayerDetectionZone = PlayerDetectionZone.new()
-		#zone.activate_distance = bounding_radius
-		#zone.deactivate_distance = bounding_radius + 100
+		var zone : PlayerDetectionZone = PlayerDetectionZone.new()
+		zone.activate_distance = bounding_radius
+		zone.deactivate_distance = bounding_radius + 100
 		
-		#sector_node.add_child(zone)
-		#sector_node.zone = zone
+		sector_node.add_child(zone)
+		sector_node.zone = zone
 		
-		#zone.position = average
+		zone.global_position = sector_node.global_position
 		
-		#zone.owner = get_tree().edited_scene_root
+		zone.owner = get_tree().edited_scene_root
 		
 		#print(i.positions)
--- Game/oddity/resources/resources/mineable/MineableSector.gd
+++ Game/oddity/resources/resources/mineable/MineableSector.gd
@@ -13,8 +13,8 @@ var spawn_radius : float
 
 var timer : Timer = Timer.new()
 
-#@export
-#var mineable_resource : MineableResource
+@export
+var mineable_resource : MineableResource
 
 @export
 var mineable_spawn_root : Node3D
@@ -38,22 +38,24 @@ func _ready() -> void:
 func _timer_timeout() -> void:
 	var player : Player = get_tree().get_first_node_in_group("Player")
 	
-	print("test")
+	#print("test")
 	
 	if player == null:
 		return
 		
 	
+	#spawn_mineables(get_nearby_instances(to_local(player.global_position), spawn_radius))
 	spawn_mineables(get_nearby_instances(to_local(player.global_position), spawn_radius))
-	
+
 	#var transformed_player_pos : Vector3 = global_position - player_pos
 	
-func _on_player_area_entered() -> void:
+func _on_player_area_entered(player : Player, control_entity : ControlEntity) -> void:
 	print("ENTER")
 	timer.start()
 	
-func _on_player_area_exited() -> void:
+func _on_player_area_exited(player : Player, control_entity : ControlEntity) -> void:
 	timer.stop()
+	despawn_mineables()
 
 func get_nearby_instances(center: Vector3, radius: float) -> Array:
 	var result : Array = []
@@ -64,6 +66,9 @@ func get_nearby_instances(center: Vector3, radius: float) -> Array:
 		var mineable : MineableTransform = mineable_transforms[i]
 		if center.distance_squared_to(mineable.mineable_transform.origin) <= r2:
 			result.append(mineable)
+			
+	print(result)
+	
 	return result
 
 func spawn_mineables(array : Array) -> void:
@@ -71,12 +76,16 @@ func spawn_mineables(array : Array) -> void:
 		if m.is_mineable_spawned:
 			continue
 		
-		#var mineable : PackedScene = mineable_resource.variants.pick_random()
+		var mineable : PackedScene = mineable_resource.variants.pick_random()
 		
-		#mineable_spawn_root.add_child(mineable.instantiate())
+		mineable_spawn_root.add_child(mineable.instantiate())
 		
 		print("spawned mineable")
 		m.is_mineable_spawned = true
+
+func despawn_mineables() -> void:
+	for i : Node in mineable_spawn_root.get_children():
+		i.queue_free()
 
 func append_transform(t : Transform3D) -> void:
 	var mineable_transform : MineableTransform = MineableTransform.new()
