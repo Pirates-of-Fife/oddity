334bd87eb024ac730f7b24845268263de916cb20
Zoe <93198038+ZoeMcFife@users.noreply.github.com>
2025-08-31 16:38:27 +0200

    Revert "added trade information"
    This reverts commit 7ec01d7c2b714aa755546c081464da9b04e64b46.

--- Game/oddity/classes/module/component/component-types/AlcubierreDrive.gd
+++ Game/oddity/classes/module/component/component-types/AlcubierreDrive.gd
@@ -81,9 +81,6 @@ func _initialize_collision_shape() -> void:
 
 
 func _on_timer_timeout() -> void:
-	if module_slot == null:
-		return
-
 	if (module_slot.vehicle != null):
 		var ship : Starship = module_slot.vehicle
 		
