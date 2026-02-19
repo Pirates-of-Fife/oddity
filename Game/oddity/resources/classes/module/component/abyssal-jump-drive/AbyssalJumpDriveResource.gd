cbe6edf24f78c0d418a24dc9fa357b64eec4073b
Zoe <93198038+ZoeMcFife@users.noreply.github.com>
2026-02-19 11:22:29 +0100

    added abyss drive resource with jump range property

---
+++ Game/oddity/resources/classes/module/component/abyssal-jump-drive/AbyssalJumpDriveResource.gd
@@ -0,0 +1,8 @@
+extends ComponentResource
+
+class_name AbyssalJumpDriveResource
+
+@export_category("Abyssal Jump Drive")
+
+@export_range(0, 30, 0.1, "or_greater", "suffix:ly")
+var jump_range : float
