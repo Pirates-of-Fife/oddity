503aabd6bd5f0e7ab1ececc2a20e83539811b849
Zoe <93198038+ZoeMcFife@users.noreply.github.com>
2026-02-23 10:33:27 +0100

    added music settings

--- Game/oddity/globals/globals.gd
+++ Game/oddity/globals/globals.gd
@@ -6,6 +6,12 @@ var creature_controller_sensitivity : float = 0.001
 @export
 var starship_controller_sensitity : float = 0.1
 
+@export
+var music_volume : float = 1
+
+@export
+var music_disabled : float = false
+
 const SETTINGS_FILE: String = "user://settings.cfg"
 
 const STARSHIP_SAVED_LOADOUT : String = "user://saved_loadout.tres"
@@ -22,6 +28,8 @@ func save_settings() -> void:
 
 	config.set_value("mouse", "creature_controller_sensitivity", creature_controller_sensitivity)
 	config.set_value("mouse", "starship_controller_sensitity", starship_controller_sensitity)
+	config.set_value("audio", "music_volume", music_volume)
+	config.set_value("audio", "music_disabled", music_disabled)
 
 	config.save(SETTINGS_FILE)
 
@@ -32,3 +40,7 @@ func load_settings() -> void:
 
 	creature_controller_sensitivity = config.get_value("mouse", "creature_controller_sensitivity", 0.001)
 	starship_controller_sensitity = config.get_value("mouse", "starship_controller_sensitity", 0.1)
+	music_volume = config.get_value("audio", "music_volume", 1)
+	music_disabled = config.get_value("audio", "music_disabled", false)
+
+	
