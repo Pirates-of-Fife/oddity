775bbbc547583bf419c949a479332afaba5991d3
Zoe McFife <93198038+ZoeMcFife@users.noreply.github.com>
2026-07-30 16:50:36 +0200

    www

--- Game/oddity/classes/module/weapon/Weapon.gd
+++ Game/oddity/classes/module/weapon/Weapon.gd
@@ -24,17 +24,9 @@ func _ready() -> void:
 	
 func _weapon_ready() -> void:
 	_module_ready()
-	
-	inserted.connect(_on_weapon_inserted)
-	
+		
 	can_be_picked_up = true
 
-func _on_weapon_inserted(slot : ModuleSlot) -> void:
-	if slot is Hardpoint:
-		if slot.size == ModuleSize.HardpointSize.SIZE_6 and size == ModuleSize.HardpointSize.SIZE_5:
-			global_position.z = slot.global_position.z + -0.336
-			global_position.y = slot.global_position.y + -0.268	
-	
 func shoot() -> void:
 	pass
 
--- Game/oddity/scenes/modules/test-module/mining-laser/size-5/size_5_mining_laser.gd
+++ Game/oddity/scenes/modules/test-module/mining-laser/size-5/size_5_mining_laser.gd
@@ -1,7 +1,1 @@
 extends MiningLaser
-
-func _on_weapon_inserted(slot : ModuleSlot) -> void:
-	if slot is Hardpoint:
-		if slot.size == ModuleSize.HardpointSize.SIZE_6 and size == ModuleSize.HardpointSize.SIZE_5:
-			global_position.z = slot.global_position.z + -0.302
-			global_position.y = slot.global_position.y + -0.516
