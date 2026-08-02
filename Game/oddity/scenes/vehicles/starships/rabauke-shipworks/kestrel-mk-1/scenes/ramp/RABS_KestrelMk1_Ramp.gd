626254ad8fbc1a63d5c6a8f8df8317de8f7ecfe5
Zoe McFife <cmdrprotogen27@gmail.com>
2026-08-02 13:56:51 +0200

    ramp effects

--- Game/oddity/scenes/vehicles/starships/rabauke-shipworks/kestrel-mk-1/scenes/ramp/RABS_KestrelMk1_Ramp.gd
+++ Game/oddity/scenes/vehicles/starships/rabauke-shipworks/kestrel-mk-1/scenes/ramp/RABS_KestrelMk1_Ramp.gd
@@ -1,3 +1,30 @@
 extends Openable
 
 class_name RABS_KestrelMk1_Ramp
+
+var initial_direction : Vector3
+
+func _ready() -> void:
+	super._ready()
+	
+	initial_direction = $GPUParticles3D.process_material.get("direction")
+
+func _process(delta: float) -> void:
+	$GPUParticles3D.process_material.set("direction", initial_direction * global_basis.inverse())
+	$GPUParticles3D2.process_material.set("direction", initial_direction * global_basis.inverse())
+
+func _on_openable_opening() -> void:
+	$GPUParticles3D.emitting = true
+	$GPUParticles3D2.emitting = true
+
+func _on_openable_closing() -> void:
+	$GPUParticles3D.emitting = true
+	$GPUParticles3D2.emitting = true
+
+func _on_openable_closed() -> void:
+	$GPUParticles3D.emitting = false
+	$GPUParticles3D2.emitting = false
+
+func _on_openable_opened() -> void:
+	$GPUParticles3D.emitting = false
+	$GPUParticles3D2.emitting = false
