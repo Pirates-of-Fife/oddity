da8dd43336a4b5dfb46c70422a8e184e02f88c62
Zoe McFife <cmdrprotogen27@gmail.com>
2026-08-23 15:50:19 +0200

    aaaaa

--- Game/oddity/classes/multi-function-display/navigation-bars/NavigationBars.gd
+++ Game/oddity/classes/multi-function-display/navigation-bars/NavigationBars.gd
@@ -6,26 +6,42 @@ class_name NavigationBars
 var ship : Starship
 
 func calculate_angle() -> float:
-	var gravity_vector : Vector3  = -ship.relative_gravity_direction
-	var ship_forwards_vector : Vector3 = Vector3(0, 0, 1)# * ship.global_basis.inverse()
+	var gravity_vector : Vector3  = ship.to_local(-ship.relative_gravity_direction)
+	var ship_up_vector : Vector3 = Vector3(0, 1, 0) * ship.global_basis.inverse()
 	
-	var ange : float = ship.global_position.angle_to(ship.active_frame_of_reference.global_position)
-	var pitch : float = ship.global_basis.z.angle_to(ship.active_frame_of_reference.global_position - ship.global_position)
+	var facing_down : bool = rad_to_deg(ship_up_vector.angle_to(ship.global_position - ship.active_frame_of_reference.global_position)) >= 90
 	
-	
-	var roll : float = ship.global_basis.x.angle_to(ship.global_position - ship.active_frame_of_reference.global_position) - PI/2
+	print(rad_to_deg(ship_up_vector.angle_to(ship.global_position - ship.active_frame_of_reference.global_position)))
 		
-	return roll
+	if facing_down:
+		return ship.global_basis.x.angle_to(ship.active_frame_of_reference.global_position - ship.global_position) - PI / 2
 	
+	return ship.global_basis.x.angle_to(ship.global_position - ship.active_frame_of_reference.global_position) - PI / 2
+	
+
+func calculate_pitch() -> float:
+	return ship.global_basis.z.angle_to(ship.global_position - ship.active_frame_of_reference.global_position) - PI / 2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if ship.current_state != ship.State.POWER_ON:
+		hide()
+		return
+
 	if ship.active_frame_of_reference == null:
+		hide()
 		return
 		
 	if !(ship.active_frame_of_reference is GravityWell):
+		hide()
 		return
-	
-	print(rad_to_deg(calculate_angle()))
+		
+	show()
 	
 	$Sprite3D.rotation.z = calculate_angle()
+	$Sprite3D2.position.y = normalize(calculate_pitch(), -0.1, 0.1, -PI/2, PI/2)
+	$Sprite3D2/Label3D.text = str(roundf(-rad_to_deg(calculate_pitch())))
+	
+
+func normalize(x : float, a : float, b : float, min_x : float, max_x : float) -> float:
+	return (b - a) * ((x - min_x) / (max_x - min_x)) + a
